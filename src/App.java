56fd073963f0bc31880e1df94f66d50d8ab46642
稲見雅一 <inami@infrastructure.blue>
2024-09-13 14:30:16 +0900

    マージ済

--- src/App.java
+++ src/App.java
@@ -7,5 +7,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello!!");
         System.out.println("testetst");
+        // add vc
+        System.out.println("test2");
     }
 }
